f044d3c6b9eef0516b9dccb71bf89e99ea31321e
Erin Staugaard <astaugaard@icloud.com>
2025-10-21 12:48:45 -0400

    wolfram alpha

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -25,13 +25,14 @@ with lib;
       qrcp
       (makeDesktopItem rec {
         name = "bluetui-desktop";
-        desktopName = "bluetui";
+        desktopName = "Bluetui";
         exec = "${pkgs.kitty}/bin/kitty ${pkgs.bluetui}/bin/bluetui";
         icon = "bluetooth";
       })
+
       (makeDesktopItem rec {
         name = "pairdrop";
-        desktopName = "pairdrop";
+        desktopName = "Pairdrop";
         exec = "${pkgs.chromium}/bin/chromium --app=\"https://pairdrop.net\"";
         icon = pkgs.fetchurl {
           url = "https://github.com/schlagmichdoch/PairDrop/raw/master/public/images/android-chrome-512x512.png";
@@ -41,8 +42,19 @@ with lib;
       })
 
       (makeDesktopItem rec {
-        name = "Flathub";
-        desktopName = "flathub";
+        name = "wolfram_alpha";
+        desktopName = "Wolfram Alpha";
+        exec = "${pkgs.chromium}/bin/chromium --app=\"https://www.wolframalpha.com/\"";
+        icon = pkgs.fetchurl {
+          url = "https://img.icons8.com/?size=96&id=13667&format=png";
+          hash = "sha256-gobtgTx9a/uBRcVBCgxMW95ebGHbGx3jLzBfyvbwbk0=";
+          name = "wolfram-alpha-icon";
+        };
+      })
+
+      (makeDesktopItem rec {
+        name = "flathub";
+        desktopName = "Flathub";
         exec = "${pkgs.chromium}/bin/chromium --app=\"https://flathub.org\"";
         icon = pkgs.fetchurl {
           url = "https://raw.githubusercontent.com/flathub-infra/assets/d593e566db996ec738621f073a13c534f681f291/web/favicon-i.svg";
