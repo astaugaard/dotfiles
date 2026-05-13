b2f99de908714be4dfccc1d2a2d740049516cc04
Erin Staugaard <astaugaard@icloud.com>
2026-05-12 22:12:10 -0400

    modified fish config

--- modules/fish.nix
+++ modules/fish.nix
@@ -39,62 +39,7 @@ with lib;
           echo (date "+%H:%M:%S")(set_color -b cyan; set_color -o green)$div(set_color normal; set_color -o black; set_color -b cyan) (prompt_pwd)(set_color -b magenta; set_color cyan)$div(set_color normal ; set_color -o black; set_color -b magenta) (echo $CMD_DURATION)ms(set_color normal; set_color -o black; set_color magenta)$div(set_color normal)" "
         '';
 
-        fish_greeting =
-          let
-            cowsayfiles = pkgs.writeText "options" ''
-              alpaca
-              elephant
-              luke-koala
-              supermilker
-              beavis.zen
-              elephant-in-snake
-              mech-and-cow
-              surgery
-              blowfish
-              eyes
-              meow
-              sus
-              bong
-              flaming-sheep
-              milk
-              three-eyes
-              bud-frogs
-              fox
-              moofasa
-              turkey
-              bunny
-              ghostbusters
-              moose
-              turtle
-              cheese
-              mutilated
-              tux
-              cower
-              hellokitty
-              ren
-              udder
-              kiss
-              sheep
-              vader
-              daemon
-              kitty
-              skeleton
-              vader-koala
-              default
-              koala
-              small
-              www
-              dragon
-              kosh
-              stegosaurus        
-              dragon-and-cow
-              llama
-              stimpy
-            '';
-          in
-          ''
-            ${pkgs.fortune}/bin/fortune | ${pkgs.cowsay}/bin/cowsay -f (shuf ${cowsayfiles} | head -n1)
-          '';
+        fish_greeting = "";
 
         fish_default_mode_prompt = ''
           echo ""
